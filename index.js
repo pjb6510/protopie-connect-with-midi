1ed04cb788dd8f561a680befb9f077c8295be584
Jason <jason@protopie.io>
2021-08-28 03:14:41 +0900

    Add: receive message and play piano sample

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 const easymidi = require("easymidi");
 const io = require("socket.io-client");
 const { go, filter } = require("fxjs");
+const PianoPlayer = require("./pianoPlayer");
+
+const pianoPlayer = new PianoPlayer();
+
+const notesByNumber = {
+  72: "C5",
+  71: "B4",
+  70: "Bb4",
+  69: "A4",
+  68: "Ab4",
+  67: "G4",
+  66: "Gb4",
+  65: "F4",
+  64: "E4",
+  63: "Eb4",
+  62: "D4",
+  61: "Db4",
+  60: "C4",
+  59: "B3",
+  58: "Bb3",
+  57: "A3",
+  56: "Ab3",
+  55: "G3",
+  54: "Gb3",
+  53: "F3",
+  52: "E3",
+  51: "Eb3",
+  50: "D3",
+  49: "Db3",
+  48: "C3",
+};
+
+const handleMessageReceive = (data) => {
+  try {
+    go(
+      data,
+      (data) => data.value,
+      (value) => {
+        if (!value) throw "no value";
+        return value;
+      },
+      (note) => [note[0] === "p", notesByNumber[note.slice(1)]],
+      ([isPress, note]) => {
+        if (isPress) {
+          pianoPlayer.play(note);
+          console.log(note);
+        }
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const connectSocket = (io, address) =>
   go(
@@ -30,9 +83,7 @@ const connectSocket = (io, address) =>
         .on("disconnect", () => {
           console.log("[SOCKETIO] disconnected");
         })
-        .on("ppMessage", (data) => {
-          console.log("[SOCKETIO] Receive a message from Connect", data);
-        })
+        .on("ppMessage", handleMessageReceive)
   );
 
 const socket = connectSocket(io, "http://localhost:9981");
